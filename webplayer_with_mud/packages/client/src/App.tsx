f94e397da91b60c2ae10461fe8c4ed0a43399ab7
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-11-07 18:04:38 +0800

    11

--- webplayer_with_mud/packages/client/src/App.tsx
+++ webplayer_with_mud/packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import {useMUD} from "./MUDContext";
 
 const styleUnset = {all: "unset"} as const;
+import React, { useEffect } from 'react';
 
 export const App = () => {
     const {
@@ -14,12 +15,86 @@ export const App = () => {
         return records;
     });
 
-    useEffect(() => {
-        // 在组件挂载完成后执行的代码
-        // 类似于 document.ready
+    function unityShowBanner(msg, type) {
+        function updateBannerVisibility() {
+            warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+        }
+        var div = document.createElement('div');
+        div.innerHTML = msg;
+        warningBanner.appendChild(div);
+        if (type == 'error') div.style = 'background: red; padding: 10px;';
+        else {
+            if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
+            setTimeout(function() {
+                warningBanner.removeChild(div);
+                updateBannerVisibility();
+            }, 5000);
+        }
+        updateBannerVisibility();
+    }
 
-        // 在这里可以进行需要在组件挂载完成后执行的逻辑
-        alert(123)
+    useEffect(() => {
+        var container = document.querySelector("#unity-container");
+        var canvas = document.querySelector("#unity-canvas");
+        var loadingBar = document.querySelector("#unity-loading-bar");
+        var progressBarFull = document.querySelector("#unity-progress-bar-full");
+        var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+        var warningBanner = document.querySelector("#unity-warning");
+
+
+        var buildUrl = "Build";
+        var loaderUrl = buildUrl + "/dojocardbuild.loader.js";
+        var config = {
+            dataUrl: buildUrl + "/dojocardbuild.data",
+            frameworkUrl: buildUrl + "/dojocardbuild.framework.js",
+            codeUrl: buildUrl + "/dojocardbuild.wasm",
+            streamingAssetsUrl: "StreamingAssets",
+            companyName: "DefaultCompany",
+            productName: "My project",
+            productVersion: "1.09",
+            showBanner: unityShowBanner,
+        };
+
+
+        if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+            // Mobile device style: fill the whole browser client area with the game canvas:
+
+            var meta = document.createElement('meta');
+            meta.name = 'viewport';
+            meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+            document.getElementsByTagName('head')[0].appendChild(meta);
+            container.className = "unity-mobile";
+            canvas.className = "unity-mobile";
+
+            // To lower canvas resolution on mobile devices to gain some
+            // performance, uncomment the following line:
+            // config.devicePixelRatio = 1;
+
+            unityShowBanner('WebGL builds are not supported on mobile devices.');
+        } else {
+            // Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
+
+            canvas.style.width = "100vw";
+            canvas.style.height = "100vh";
+        }
+
+
+        var script = document.createElement("script");
+        script.src = loaderUrl;
+        script.onload = () => {
+            createUnityInstance(canvas, config, (progress) => {
+                progressBarFull.style.width = 100 * progress + "%";
+            }).then((unityInstance) => {
+                loadingBar.style.display = "none";
+                // fullscreenButton.onclick = () => {
+                //     unityInstance.SetFullscreen(1);
+                // };
+            }).catch((message) => {
+                alert(message);
+            });
+        };
+        document.body.appendChild(script);
+
         return () => {
             // 在组件卸载时执行的清理操作
             // 类似于 componentWillUnmount
