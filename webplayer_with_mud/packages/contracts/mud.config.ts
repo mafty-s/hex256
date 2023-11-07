15c498d94e9bff98de40b92597ac280ff1291b99
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-11-07 17:35:18 +0800

    1

---
+++ webplayer_with_mud/packages/contracts/mud.config.ts
@@ -0,0 +1,18 @@
+import { mudConfig } from "@latticexyz/world/register";
+
+export default mudConfig({
+  tables: {
+    Cards:{
+      valueSchema: {
+        createdAt: "uint256",
+      },
+    }
+    Tasks: {
+      valueSchema: {
+        createdAt: "uint256",
+        completedAt: "uint256",
+        description: "string",
+      },
+    },
+  },
+});
