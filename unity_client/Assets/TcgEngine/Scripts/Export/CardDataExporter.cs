87a1477b108cbeb34832b9bde2036571cc968f44
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-11-08 18:12:44 +0800

    add import

--- unity_client/Assets/TcgEngine/Scripts/Export/CardDataExporter.cs
+++ unity_client/Assets/TcgEngine/Scripts/Export/CardDataExporter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
-using Unity.VisualScripting;
 
 namespace TcgEngine
 {
@@ -26,7 +25,7 @@ namespace TcgEngine
             {
                 // 写入表头
                 sw.WriteLine(
-                    "ID,Title,Type,Team,Rarity,Mana,Attack,HP,Traits,Abilities,Text,Description,DeckBuilding,Cost,Packs,ArtFull,ArtBoard");
+                    "ID,Title,Type,Team,Rarity,Mana,Attack,HP,Traits,Abilities,Text,Description,DeckBuilding,Cost,Packs,ArtFull,ArtBoard,SpawnFX,DeathFX,AttackFX,DamageFX,IdleFX,SpawnAudio,DeathAudio,AttackAudio,DamageAudio");
 
                 // 写入数据行
                 foreach (CardData cardData in cardList)
@@ -59,8 +58,19 @@ namespace TcgEngine
                     string art_full = (cardData.art_full != null) ? AssetDatabase.GetAssetPath(cardData.art_full) : "";
                     string art_board = (cardData.art_board != null) ? AssetDatabase.GetAssetPath(cardData.art_board) : "";
 
+                    string spawn_fx = (cardData.spawn_fx != null) ? AssetDatabase.GetAssetPath(cardData.spawn_fx) : "";
+                    string death_fx = (cardData.death_fx != null) ? AssetDatabase.GetAssetPath(cardData.death_fx) : "";
+                    string attack_fx = (cardData.attack_fx != null) ? AssetDatabase.GetAssetPath(cardData.attack_fx) : "";
+                    string damage_fx = (cardData.damage_fx != null) ? AssetDatabase.GetAssetPath(cardData.damage_fx) : "";
+                    string idle_fx = (cardData.idle_fx != null) ? AssetDatabase.GetAssetPath(cardData.idle_fx) : "";
+
+                    string spawn_audio = (cardData.spawn_audio != null) ? AssetDatabase.GetAssetPath(cardData.spawn_audio) : "";
+                    string death_audio = (cardData.death_audio != null) ? AssetDatabase.GetAssetPath(cardData.death_audio) : "";
+                    string attack_audio = (cardData.attack_audio != null) ? AssetDatabase.GetAssetPath(cardData.attack_audio) : "";
+                    string damage_audio = (cardData.damage_audio != null) ? AssetDatabase.GetAssetPath(cardData.damage_audio) : "";
                     
-                    sw.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},\"{10}\",{11},{12},{13},{14},{15},{16}",                        cardData.id,
+                    sw.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},\"{10}\",{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{25}",
+                        cardData.id,
                         cardData.title,
                         cardData.type.ToString(),
                         cardData.team.name,
@@ -76,8 +86,16 @@ namespace TcgEngine
                         cardData.cost,
                         packs,
                         art_full,
-                        art_board
-                        ));
+                        art_board,
+                        spawn_fx,
+                        death_fx,
+                        attack_fx,
+                        damage_fx,
+                        idle_fx,
+                        spawn_audio,
+                        death_audio,
+                        attack_audio,
+                        damage_audio));
                 }
             }
 
