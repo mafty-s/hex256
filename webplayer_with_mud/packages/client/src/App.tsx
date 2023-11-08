d665d6cfff86283d3134c140a24f75e2a4d7c6f3
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2023-11-08 13:00:32 +0800

    add web player

--- webplayer_with_mud/packages/client/src/App.tsx
+++ webplayer_with_mud/packages/client/src/App.tsx
@@ -34,6 +34,8 @@ export const App = () => {
     }
 
     useEffect(() => {
+
+        window.addTask = addTask;
         var container = document.querySelector("#unity-container");
         var canvas = document.querySelector("#unity-canvas");
         var loadingBar = document.querySelector("#unity-loading-bar");
@@ -123,91 +125,95 @@ export const App = () => {
                 {/*</div>*/}
             </div>
 
-            <table>
-                <tbody>
-                {tasks.map((task) => (
-                    <tr key={task.id}>
-                        <td align="right">
-                            <input
-                                type="checkbox"
-                                checked={task.value.completedAt > 0n}
-                                title={task.value.completedAt === 0n ? "Mark task as completed" : "Mark task as incomplete"}
-                                onChange={async (event) => {
-                                    event.preventDefault();
-                                    const checkbox = event.currentTarget;
+            <div class="mud_devtool">
+                <table>
+                    <tbody>
+                    {tasks.map((task) => (
+                        <tr key={task.id}>
+                            <td align="right">
+                                <input
+                                    type="checkbox"
+                                    checked={task.value.completedAt > 0n}
+                                    title={task.value.completedAt === 0n ? "Mark task as completed" : "Mark task as incomplete"}
+                                    onChange={async (event) => {
+                                        event.preventDefault();
+                                        const checkbox = event.currentTarget;
 
-                                    checkbox.disabled = true;
-                                    try {
-                                        await toggleTask(task.key.key);
-                                    } finally {
-                                        checkbox.disabled = false;
-                                    }
-                                }}
-                            />
+                                        checkbox.disabled = true;
+                                        try {
+                                            await toggleTask(task.key.key);
+                                        } finally {
+                                            checkbox.disabled = false;
+                                        }
+                                    }}
+                                />
+                            </td>
+                            <td>{task.value.completedAt > 0n ?
+                                <s>{task.value.description}</s> : <>{task.value.description}</>}</td>
+                            <td align="right">
+                                <button
+                                    type="button"
+                                    title="Delete task"
+                                    style={styleUnset}
+                                    onClick={async (event) => {
+                                        event.preventDefault();
+                                        if (!window.confirm("Are you sure you want to delete this task?")) return;
+
+                                        const button = event.currentTarget;
+                                        button.disabled = true;
+                                        try {
+                                            await deleteTask(task.key.key);
+                                        } finally {
+                                            button.disabled = false;
+                                        }
+                                    }}
+                                >
+                                    &times;
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td>
+                            <input type="checkbox" disabled/>
                         </td>
-                        <td>{task.value.completedAt > 0n ?
-                            <s>{task.value.description}</s> : <>{task.value.description}</>}</td>
-                        <td align="right">
-                            <button
-                                type="button"
-                                title="Delete task"
-                                style={styleUnset}
-                                onClick={async (event) => {
+                        <td colSpan={2}>
+                            <form
+                                onSubmit={async (event) => {
                                     event.preventDefault();
-                                    if (!window.confirm("Are you sure you want to delete this task?")) return;
+                                    const form = event.currentTarget;
+                                    const fieldset = form.querySelector("fieldset");
+                                    if (!(fieldset instanceof HTMLFieldSetElement)) return;
 
-                                    const button = event.currentTarget;
-                                    button.disabled = true;
+                                    const formData = new FormData(form);
+                                    const desc = formData.get("description");
+                                    if (typeof desc !== "string") return;
+
+                                    fieldset.disabled = true;
                                     try {
-                                        await deleteTask(task.key.key);
+                                        await addTask(desc);
+                                        form.reset();
                                     } finally {
-                                        button.disabled = false;
+                                        fieldset.disabled = false;
                                     }
                                 }}
                             >
-                                &times;
-                            </button>
+                                <fieldset style={styleUnset}>
+                                    <input type="text" name="description"/>{" "}
+                                    <button type="submit" title="Add task">
+                                        Add
+                                    </button>
+                                </fieldset>
+                            </form>
                         </td>
                     </tr>
-                ))}
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td>
-                        <input type="checkbox" disabled/>
-                    </td>
-                    <td colSpan={2}>
-                        <form
-                            onSubmit={async (event) => {
-                                event.preventDefault();
-                                const form = event.currentTarget;
-                                const fieldset = form.querySelector("fieldset");
-                                if (!(fieldset instanceof HTMLFieldSetElement)) return;
+                    </tfoot>
+                </table>
+            </div>
 
-                                const formData = new FormData(form);
-                                const desc = formData.get("description");
-                                if (typeof desc !== "string") return;
 
-                                fieldset.disabled = true;
-                                try {
-                                    await addTask(desc);
-                                    form.reset();
-                                } finally {
-                                    fieldset.disabled = false;
-                                }
-                            }}
-                        >
-                            <fieldset style={styleUnset}>
-                                <input type="text" name="description"/>{" "}
-                                <button type="submit" title="Add task">
-                                    Add
-                                </button>
-                            </fieldset>
-                        </form>
-                    </td>
-                </tr>
-                </tfoot>
-            </table>
         </>
     )
         ;
