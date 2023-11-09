22dac7151850a5175cb9d9bf5a1a4abeb24dcfb6
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-11-10 04:00:46 +0800

    1

--- unity_client/Assets/TcgEngine/Scripts/Network/TcgTransport.cs
+++ unity_client/Assets/TcgEngine/Scripts/Network/TcgTransport.cs
@@ -35,14 +35,7 @@
 // }
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-// using System.Net.WebSockets;
 using System.Threading;
-using System.Threading.Tasks;
-using TcgEngine.Client;
-using Unity.Netcode;
 using UnityEngine;
 using UnityWebSocket;
 
@@ -65,39 +58,24 @@ namespace TcgEngine
 
         public virtual void Init()
         {
+
             Debug.Log("Websocket Init");
+            string url = "ws://" + "localhost" + ":" + "8777";
+            webSocket = new WebSocket(url); 
+            webSocket.OnError += WebSocketError;
+            webSocket.OnOpen += WebSocketOpen;
             isConnected = false;
             // webSocket = new ClientWebSocket();
             // cancellationTokenSource = new CancellationTokenSource();
         }
-
-        // public virtual void SetServer(ushort port)
-        // {
-        //     // 启动本地 WebSocket 服务端
-        //     Uri serverUri = new Uri($"{defaultServerAddress}:{port}");
-        //     Debug.Log("Websocket Server Starting...");
-        //
-        //     try
-        //     {
-        //         // 启动 WebSocket 服务端的逻辑
-        //         // 这里可以使用第三方库或自己实现 WebSocket 服务端的逻辑
-        //         // 例如：WebSocketServer.Start(serverUri);
-        //         Debug.Log("Websocket Server Started");
-        //         isConnected = true;
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         Debug.LogError("WebSocket server startup error: " + e.Message);
-        //     }
-        // }
 
 
         public void SendMessageByte(byte[] writer)
         {
             if (webSocket == null) return;
             // 发送消息给服务端
-            if (isConnected)
-            {
+            // if (isConnected)
+            // {
                 try
                 {
                     webSocket.SendAsync(writer);
@@ -107,11 +85,11 @@ namespace TcgEngine
                 {
                     Debug.LogError("Failed to send message to server: " + e.Message);
                 }
-            }
-            else
-            {
-                Debug.LogError("WebSocket connection is not open");
-            }
+            // }
+            // else
+            // {
+            //     Debug.LogError("WebSocket connection is not open");
+            // }
         }
 
         WebSocket webSocket;
@@ -137,7 +115,7 @@ namespace TcgEngine
         {
             webSocket.OnOpen += (sender, e) =>
             {
-                this.isConnected = true;
+                Debug.Log("TcgTransport OnOpen");
                 callback();
             };
         }
@@ -150,20 +128,12 @@ namespace TcgEngine
                 callback();
             };
         }
-
-
+        
         public virtual void SetClient(string address, ushort port)
         {
-            string url = "ws://" + "localhost" + ":" + port;
-            Debug.Log("Websocket Client Starting..." + url);
+            Debug.Log("Websocket Client Starting..." );
             try
             {
-                webSocket = new WebSocket(url);
-                // webSocket.OnOpen += WebSocketOpen;
-                webSocket.OnError += WebSocketError;
-                // webSocket.OnClose += WebSocketClose;
-                // webSocket.OnMessage += WebSocketReceive;
-
                 webSocket.ConnectAsync();
             }
             catch (Exception e)
@@ -172,59 +142,22 @@ namespace TcgEngine
             }
         }
 
-        // private void WebSocketReceive(object sender, MessageEventArgs e)
-        // {
-        //
-        //     if (e.IsText)
-        //     {
-        //         Debug.Log(e.Data);
-        //     
-        //     }
-        //     else if (e.IsBinary)
-        //     {
-        //         Debug.LogError(string.Format("Bytes ({1}): {0}", e.Data, e.RawData.Length));
-        //         
-        //         byte[] payload = e.RawData; // 假设 message.data 是字节数组类型
-        //
-        //         int length = BitConverter.ToInt32(payload, 0); // 获取长度
-        //         string type = Encoding.UTF8.GetString(payload, 4, length); // 假设类型占用4个字节，从第5个字节开始
-        //         int contentLength = payload.Length - 4 - length; // 计算内容的长度
-        //         byte[] content = new byte[contentLength];
-        //         Array.Copy(payload, 4 + length, content, 0, contentLength); 
-        //     
-        //         Debug.Log("Length: " + length);
-        //         Debug.Log("Type: " + type);
-        //         Debug.Log("Content: " + content);
-        //         
-        //         //todo
-        //         
-        //     }
-        // }
-
-        // private void WebSocketOpen(object sender, OpenEventArgs e)
-        // {
-        //     Debug.Log("Websocket Connected");
-        //     this.isConnected = true;
-        //     // string uid = Guid.NewGuid().ToString();
-        //     // GameClient.Get().ConnectToGame2(uid);
-        // }
-
         private void WebSocketError(object sender, ErrorEventArgs e)
         {
             Debug.LogError(string.Format("Websocket WebSocketError: Reason: {0}", e.Message));
             isConnected = false;
         }
+        
+        private void WebSocketOpen(object sender, OpenEventArgs e)
+        {
+            Debug.Log("Websocket Connected");
+       
+        }
 
-        // private void WebSocketClose(object sender, CloseEventArgs e)
-        // {
-        //     isConnected = false;
-        //     Debug.LogError(string.Format("网络已断开: StatusCode: {0}, Reason: {1}", e.StatusCode, e.Reason));
-        // }
 
         public virtual void Close()
         {
-            // cancellationTokenSource.Cancel();
-            // webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None).Wait();
+            webSocket.CloseAsync();
         }
 
         public virtual string GetAddress()
