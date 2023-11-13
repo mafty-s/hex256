9ad2127d5032082a134d3a2633b8a115b9b0749b
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-11-13 20:28:12 +0800

    player

---
+++ webplayer_with_mud/packages/contracts/src/systems/ConfigSystem.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import {System} from "@latticexyz/world/src/System.sol";
+import {Users, UsersData} from "../codegen/index.sol";
+
+contract ConfigSystem is System {
+
+    // 构造函数
+    constructor () {
+
+    }
+
+
+}
