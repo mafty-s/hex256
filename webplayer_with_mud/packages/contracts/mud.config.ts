ac8bce84c004c3637a7e188eddece42a540bf11e
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-11-13 13:38:57 +0800

    user on chian

--- webplayer_with_mud/packages/contracts/mud.config.ts
+++ webplayer_with_mud/packages/contracts/mud.config.ts
@@ -5,24 +5,26 @@ export default mudConfig({
     Users:{
       valueSchema: {
         id: "uint256",
-        avatar: "string",
-        cardback: "string",
         coin: "uint256",
         xp: "uint256",
         createdAt: "uint256",
+        cards: "uint256[]",
+        packs: "uint256[]",
+        // avatar: "string",
+        // cardback: "string",
       }
     },
     Cards:{
       valueSchema: {
-        tid: "string",
-        type: "string",
-        team: "string",
-        rarity: "string",
         mana: "uint256",
         attack: "uint256",
         hp: "uint256",
         cost: "uint256",
         createdAt: "uint256",
+        // tid: "string",
+        // type: "string",
+        // team: "string",
+        // rarity: "string",
       },
     },
     Tasks: {
