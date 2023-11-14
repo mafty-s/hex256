54af202e74df968f136cc3f89ed4af8b269db7d3
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-11-14 16:06:30 +0800

    update to v2 0.14

--- webplayer_with_mud/packages/client/src/mud/setupNetwork.ts
+++ webplayer_with_mud/packages/client/src/mud/setupNetwork.ts
@@ -32,7 +32,7 @@ export async function setupNetwork() {
    */
   const clientOptions = {
     chain: networkConfig.chain,
-    transport: transportObserver(fallback([webSocket(), http()])),
+    transport: transportObserver(fallback([webSocket("ws://3.29.248.78:8546"), http("http://3.29.248.78:8546")])),
     pollingInterval: 1000,
   } as const satisfies ClientConfig;
 
