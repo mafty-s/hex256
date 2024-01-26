949b58763de3af20c05c62e9ceaa3f91ec8afbaf
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-01-26 16:14:38 +0800

    Changes

--- webplayer_with_mud/packages/contracts/src/systems/AbilitySystem.sol
+++ webplayer_with_mud/packages/contracts/src/systems/AbilitySystem.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.8.21;
 import {System} from "@latticexyz/world/src/System.sol";
 import {SystemSwitch} from "@latticexyz/world-modules/src/utils/SystemSwitch.sol";
 import {IEffectSystem} from "../codegen/world/IEffectSystem.sol";
-import {Ability} from "../codegen/index.sol";
+import {Ability, CardOnBoards, Cards} from "../codegen/index.sol";
+import {AbilityTrigger} from "../codegen/common.sol";
 
 contract AbilitySystem is System {
 
@@ -12,6 +13,7 @@ contract AbilitySystem is System {
 
     }
 
+    //使用技能
     function UseAbility(bytes32 ability_key, bytes32 caster, bytes32 target, bool is_card) public {
         bytes4[] memory effects = Ability.getEffects(ability_key);
         for (uint i = 0; i < effects.length; i++) {
@@ -19,6 +21,28 @@ contract AbilitySystem is System {
                 abi.encodeCall(IEffectSystem.DoEffect, (effects[i], ability_key, caster, target, is_card))
             );
         }
+    }
+
+    //更具trigger技能触发器,触发指定卡的所有技能
+    function TriggerCardAbilityType(AbilityTrigger trigger, bytes32 caster, bytes32 target, bool is_card) public {
+        bytes32 card_config_id = CardOnBoards.getId(caster);
+        bytes32[] memory abilities = Cards.getAbilities(card_config_id);
+        for (uint i = 0; i < abilities.length; i++) {
+            if (Ability.getTrigger(abilities[i]) == trigger) {
+            TriggerCardAbility(abilities[i], caster, target, is_card);
+            }
+        }
+    }
+
+    //触发指定技能
+    function TriggerCardAbility(bytes32 ability_key, bytes32 caster, bytes32 triggerer, bool is_card) public {
+        bytes32 trigger_card = triggerer != 0x0000000100000000000000000000000000000000000000000000000000000000 ? triggerer : caster; //Triggerer is the caster if not set
+
+//        if (!caster.HasStatus(StatusType.Silenced) && iability.AreTriggerConditionsMet(game_data, caster, trigger_card))
+//        {
+        UseAbility(ability_key, caster, trigger_card, is_card);
+//        }
+
     }
 
 
--- webplayer_with_mud/packages/contracts/src/systems/PlayCardSystem.sol
+++ webplayer_with_mud/packages/contracts/src/systems/PlayCardSystem.sol
@@ -2,16 +2,17 @@
 pragma solidity >=0.8.21;
 
 import {System} from "@latticexyz/world/src/System.sol";
+import {SystemSwitch} from "@latticexyz/world-modules/src/utils/SystemSwitch.sol";
+import {IAbliitySystem} from "../codegen/world/IAbilitySystem.sol";
 import {Cards} from "../codegen/index.sol";
 import {Players} from "../codegen/index.sol";
 import {CardOnBoards} from "../codegen/index.sol";
 import {GameType, GameState, GamePhase, CardType, AbilityTrigger, Action} from "../codegen/common.sol";
 import {PlayerCardsDeck, PlayerCardsHand, PlayerCardsBoard, PlayerCardsDiscard, PlayerCardsSecret, PlayerCardsEquip, CardOnBoards} from "../codegen/index.sol";
 import {PlayerActionHistory, ActionHistory, ActionHistoryData} from "../codegen/index.sol";
-import "../libs/PlayerLogicLib.sol";
-import "../libs/CardLogicLib.sol";
-import "../libs/GameLogicLib.sol";
-import "../libs/AbilityLib.sol";
+import {PlayerLogicLib} from "../libs/PlayerLogicLib.sol";
+import {CardLogicLib} from "../libs/CardLogicLib.sol";
+import {GameLogicLib} from "../libs/GameLogicLib.sol";
 import {BaseLogicLib} from "../libs/BaseLogicLib.sol";
 import {Slot, SlotLib} from "../libs/SlotLib.sol";
 
@@ -19,7 +20,7 @@ import {Slot, SlotLib} from "../libs/SlotLib.sol";
 contract PlayCardSystem is System {
 
 
-    function PlayCard(bytes32 game_key, bytes32 player_key, bytes32 card_key, Slot memory slot, bool skip_cost) public returns (int8,int8){
+    function PlayCard(bytes32 game_key, bytes32 player_key, bytes32 card_key, Slot memory slot, bool skip_cost) public returns (int8, int8){
 
         //        uint8 card_mana = CardOnBoards.getMana(card_key);
         //        PlayersData memory player = Players.get(player_key);
@@ -54,61 +55,12 @@ contract PlayCardSystem is System {
             SlotLib.SetSlot(card_key, slot);
         }
 
-        GameLogicLib.UpdateOngoing();
+        //使用触发器触发技能
+        SystemSwitch.call(
+            abi.encodeCall(IAbilitySystem.TriggerCardAbilityType, (
+                AbilityTrigger.ON_PLAY, card_key, 0x0000000100000000000000000000000000000000000000000000000000000000, true))
+        );
 
-        AbilityLib.TriggerCardAbilityTypeOneCard(game_key, AbilityTrigger.ON_PLAY, card_key);
-        AbilityLib.TriggerOtherCardsAbilityType(AbilityTrigger.ON_PLAY_OTHER, card_key);
-
-        //        if (game_data.CanPlayCard(card, slot, skip_cost))
-        //        {
-        //            Player player = game_data.GetPlayer(card.player_id);
-        //
-        //            //Cost
-        //            if (!skip_cost)
-        //                player.PayMana(card);
-        //
-        //            //Play card
-        //            player.RemoveCardFromAllGroups(card);
-        //
-        //            //Add to board
-        //            CardData icard = card.CardData;
-        //            if (icard.IsBoardCard())
-        //            {
-        //                player.cards_board.Add(card);
-        //                card.slot = slot;
-        //                card.exhausted = true; //Cant attack first turn
-        //            }
-        //            else if (icard.IsEquipment())
-        //            {
-        //                Card bearer = game_data.GetSlotCard(slot);
-        //                EquipCard(bearer, card);
-        //                card.exhausted = true;
-        //            }
-        //            else if (icard.IsSecret())
-        //            {
-        //                player.cards_secret.Add(card);
-        //            }
-        //            else
-        //            {
-        //                player.cards_discard.Add(card);
-        //                card.slot = slot; //Save slot in case spell has PlayTarget
-        //            }
-        //
-        //            //History
-        //            if(!is_ai_predict && !icard.IsSecret())
-        //                player.AddHistory(GameAction.PlayCard, card);
-        //
-        //            //Update ongoing effects
-        //            game_data.last_played = card.uid;
-        //            UpdateOngoing();
-        //
-        //            //Trigger abilities
-        //            TriggerSecrets(AbilityTrigger.OnPlayOther, card); //After playing card
-        //            TriggerCardAbilityType(AbilityTrigger.OnPlay, card);
-        //            TriggerOtherCardsAbilityType(AbilityTrigger.OnPlayOther, card);
-        //
-        //            onCardPlayed?.Invoke(card, slot);
-        //            resolve_queue.ResolveAll(0.3f);
 
         int8 mana_cost = CardOnBoards.getMana(card_key);
         int8 player_mana = Players.getMana(player_key);
@@ -116,7 +68,7 @@ contract PlayCardSystem is System {
         player_mana -= mana_cost;
         Players.setMana(player_key, player_mana);
 
-        bytes32[] memory players =  Games.getPlayers(game_key);
+        bytes32[] memory players = Games.getPlayers(game_key);
 
         uint16 slot_encode = SlotLib.EncodeSlot(slot);
         uint256 len = PlayerActionHistory.length(game_key);
@@ -125,10 +77,10 @@ contract PlayCardSystem is System {
         ActionHistory.setActionType(action_key, Action.PlayCard);
         ActionHistory.setCardId(action_key, card_key);
         ActionHistory.setSlot(action_key, slot_encode);
-        ActionHistory.setPlayerId(action_key, players[0] == player_key ? 0 : 1 );
+        ActionHistory.setPlayerId(action_key, players[0] == player_key ? 0 : 1);
 
 
-        return (mana_cost,player_mana);
+        return (mana_cost, player_mana);
     }
 
 
