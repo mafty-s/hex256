c21004336ec4953effccde14df836c1258270e10
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-01-31 18:23:54 +0800

    Changes

--- webplayer_with_mud/packages/contracts/mud.config.ts
+++ webplayer_with_mud/packages/contracts/mud.config.ts
@@ -20,7 +20,7 @@ export default mudConfig({
         ConditionObjType: ["ConditionCardType"],
         TraitData: ["Dragon", "Growth", "SpellDamage", "Wolf"],
         EffectAttackerType: ["Self", "AbilityTriggerer", "LastPlayed", "LastTargeted"],
-        Status: ["None", "Armor", 'Attack', 'Deathtouch', 'Flying', 'Fury', 'HP', 'Intimidate', 'Invicibility', 'Lifesteal', 'Paralysed', 'Poisoned', 'Protected', 'Shell', 'Silenced', 'Sleep', 'SpellImmunity', 'Stealth', 'Taunt', 'Trample'，'Hp'],
+        Status: ["None", "Armor", 'Attack', 'Deathtouch', 'Flying', 'Fury', 'HP', 'Intimidate', 'Invicibility', 'Lifesteal', 'Paralysed', 'Poisoned', 'Protected', 'Shell', 'Silenced', 'Sleep', 'SpellImmunity', 'Stealth', 'Taunt', 'Trample','Hp'],
     },
     tables: {
         AdminSingleton: {
