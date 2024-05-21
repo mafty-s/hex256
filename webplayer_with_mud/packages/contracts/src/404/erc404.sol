af950f5d1d1ea500c2ca287db1ccc8b5fedc8583
ArichiMasaomi <145103145+ArichiMasaomi@users.noreply.github.com>
2024-05-21 16:18:45 +0800

    Update erc404.sol

--- webplayer_with_mud/packages/contracts/src/404/erc404.sol
+++ webplayer_with_mud/packages/contracts/src/404/erc404.sol
@@ -8,3 +8,44 @@ abstract contract ERC404 is Ownable {
     error InvalidRecipient();
     error InvalidSender();
     error UnsafeRecipient();
+
+    // Metadata
+    /// @dev Token name
+    string public name;
+
+    /// @dev Token symbol
+    string public symbol;
+
+    /// @dev Decimals for fractional representation
+    uint8 public immutable decimals;
+
+    /// @dev Total supply in fractionalized representation
+    uint256 public totalSupply;
+
+    /// @dev Current mint counter, monotonically increasing to ensure accurate ownership
+    uint256 public minted;
+
+    // Mappings
+    /// @dev Balance of user in fractional representation
+    mapping(address => uint256) public balanceOf;
+
+    /// @dev Allowance of user in fractional representation
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    /// @dev Approval in native representaion
+    mapping(uint256 => address) public getApproved;
+
+    /// @dev Approval for all in native representation
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+
+    /// @dev Owner of id in native representation
+    mapping(uint256 => address) internal _ownerOf;
+
+    /// @dev Array of owned ids in native representation
+    mapping(address => uint256[]) internal _owned;
+
+    /// @dev Tracks indices for the _owned mapping
+    mapping(uint256 => uint256) internal _ownedIndex;
+
+    /// @dev Addresses whitelisted from minting / burning for gas savings (pairs, routers, etc)
+    mapping(address => bool) public whitelist;
